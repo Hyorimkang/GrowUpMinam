593d88e826694c4fdd89367ebde219cbaaebd5b3
이지연 <leejiyeon415@gmail.com>
2023-06-22 18:24:34 +0900

    정리

--- Assets/Scripts/GoStudyScene.cs
+++ Assets/Scripts/GoStudyScene.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class GoStudyScene : MonoBehaviour
 {
-    public void StudyButton(){  //운동하기 화면으로 전환
+    public void StudyButton(){  //공부하기 화면으로 전환
         SceneManager.LoadScene("StudyMode");
         Debug.Log("공부하기 버튼 클릭!");
     }
