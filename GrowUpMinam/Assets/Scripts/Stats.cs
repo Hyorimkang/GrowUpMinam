9e205fd1befcd5f1c78b9b8ad4ab21ca56fb074e
이지연 <leejiyeon415@gmail.com>
2023-06-25 03:26:09 +0900

    운동하기 배경음악, 효과음 재생

---
+++ GrowUpMinam/Assets/Scripts/Stats.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stats : MonoBehaviour
+{
+    public Text Knowledge;  //지식 수준
+    public Text Weight;  //몸무게
+    public Text Level;  //단계
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
