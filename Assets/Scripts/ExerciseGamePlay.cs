2890023d43e58c21a148d017869e3901005caed6
이지연 <leejiyeon415@gmail.com>
2023-06-27 23:43:46 +0900

    파일 문제 수정

--- Assets/Scripts/ExerciseGamePlay.cs
+++ Assets/Scripts/ExerciseGamePlay.cs
@@ -12,36 +12,37 @@ public class GamePlay : MonoBehaviour
     public GameObject GameOverPanel;  //게임 종료 패널
     private List<GameObject> objectsList = new List<GameObject>();  // 생성된 오브젝트를 저장할 리스트
     private int RemoveFatCount = 0;  //제거한 지방 수
-    private float spawnDelay = 1f;  // 오브젝트 생성 딜레이
-    private float spawnTimer = 0f;  // 오브젝트 생성 타이머
     private int maxObjectCount = 5;  // 화면에 표시될 최대 오브젝트 수
     private float Timer = 31;  //30초 제한시간
     public Text TimerText;  //제한시간 텍스트
     public Text WeightLossText;  //감량 텍스트
-    public Text FatCountText;  //제거한 지방 총 개수
-    bool StartCheck = false;  //게임시작 버튼 눌렀는지 안눌렀는지
+    public Text FatCountText;  //제거한 지방 총 개수 텍스트
+    bool PlayCheck = false;  //게임시작 했는지 안했는지
 
     private void Start() {
         GameOverPanel.SetActive(false);  //게임 종료 패널 안보이게 함
+        Sounds.instance.BGM();  //브금 재생
     }
 
     public void GameStart()  //게임시작 버튼을 눌렀을 때
     {
         PlayStartPanel.SetActive(false);  //시작 패널 안보이게 함
-        SpawnInitialObjects();
-        StartCheck = true;
+        SpawnInitialObjects();  //지방 오브젝트 5개 생성
+        PlayCheck = true;
     }
 
     void Update()
     {
-        if(StartCheck)  //시작 버튼을 누르면
+        if(PlayCheck)  //게임이 시작하면
         {
             //30초 제한시간 타이머
             if((int)Timer == 0){  //제한시간 끝나면
                 TimerText.text = "제한시간:종료";
                 GameOverPanel.SetActive(true);  //게임 종료 패널 보이게 함
-                FatCountText.text = "지방 " + RemoveFatCount + "개 제거";
-                WeightLossText.text =  "총 " + WeightLoss() + "kg 감량 성공!";
+                FatCountText.text = "지방 " + RemoveFatCount + "개 제거";  //제거 개수 텍스트설정
+                WeightLossText.text =  "총 " + WeightLoss() + "kg 감량 성공!";  //감량 kg 텍스트설정
+                Stats.Weight -= WeightLoss();  //몸무게 감량
+                PlayCheck = false;  //종료
             }else{
                 Timer -= Time.deltaTime;
                 TimerText.text = "제한시간: " + (int)Timer + "초";
@@ -54,25 +55,17 @@ public class GamePlay : MonoBehaviour
 
                 if (hitCollider != null && objectsList.Contains(hitCollider.gameObject))// 오브젝트를 터치한 경우
                 {
+                    Sounds.instance.FatTouchSound();  //지방 터치 효과음
                     IncreaseScore();  // 점수 증가
                     RemoveObject(hitCollider.gameObject);  // 지방 오브젝트 제거
                     SpawnObject();  // 새로운 오브젝트 생성
                 }
             }
-            // 타이머 업데이트
-            spawnTimer += Time.deltaTime;
-
-            if (spawnTimer >= spawnDelay)
-            {
-                // 오브젝트 생성 타이머가 딜레이 이상일 때
-                spawnTimer = 0f;  // 타이머 초기화
-                SpawnObject();  // 오브젝트 생성
-            }
         }
         
     }
-    private int WeightLoss(){  //감량한 kg 계산
-        int loss = 0;  //감량 kg
+    private int WeightLoss(){  //감량한 몸무게 계산
+        int loss = 0;  //감량한 몸무게
         if(RemoveFatCount>=70)  //70개 이상이면 5kg 감량
             loss = 5;
         else if(RemoveFatCount>=50)  //50개 이상이면 3kg 감량
@@ -93,12 +86,10 @@ public class GamePlay : MonoBehaviour
 
     private void RemoveObject(GameObject obj)  //지방 오브젝트 제거
     {
-        Debug.Log("지방 제거");
         objectsList.Remove(obj);  //리스트에서 제거
         Destroy(obj);  //아예 제거
     }
-
-    private void SpawnInitialObjects()
+    private void SpawnInitialObjects()  //5개까지만 지방 오브젝트 생성
     {
         for (int i = 0; i < maxObjectCount; i++)
         {
@@ -134,9 +125,8 @@ public class GamePlay : MonoBehaviour
             objectsList.Add(newFatObject);
         }
     }
-    private Vector3 GetRandomPositionInScreen()
+    private Vector3 GetRandomPositionInScreen()// 캔버스의 사이즈를 기준으로 랜덤 위치 생성
     {
-        // 캔버스의 사이즈를 기준으로 랜덤 위치 생성
         //화면 밖으로 지방 오브젝트가 삐져나가지 않도록 랜덤 위치 범위를 스크린 크기보다 적게 설정
         float x = Random.Range(Screen.width * 0.1f, Screen.width * 0.9f);
         float y = Random.Range(Screen.height * 0.1f, Screen.height * 0.9f);
--- Assets/Scripts/MinamTouch.cs
+++ Assets/Scripts/MinamTouch.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MinamTouch : MonoBehaviour
 {
     Vector2 touchPosition;
-    string[] minamSay = {"엄마..."};  //미남이 말풍선 텍스트
+    string[] MinamSay = {"엄마...","내가 성공할 수 있을까?", "쩝쩝짭짭짭","여동생을 날 어떻게\n생각할까?","아자!아자!"};  //미남이 말풍선 텍스트
+    public Text BubbleText;  //말풍선 대사 텍스트
+    public GameObject Bubble;  //말풍선 오브젝트
     bool isClicked = false;  // 클릭 이벤트 발생 여부를 저장하는 변수
+    private float timer = 0f;  // 타이머 변수
+    private float hideDelay = 4f;  //말풍선 숨기기 지연시간
+
+    private void Start() {
+        Bubble.SetActive(false);  //말풍선 안보이게 함
+        RectTransform BubbleRectTransform = Bubble.GetComponent<RectTransform>();
+        BubbleRectTransform.position = new Vector3(1050, BubbleRectTransform.position.y, BubbleRectTransform.position.z);  //말풍선 위치
+        BubbleRectTransform.pivot = new Vector2(0f, 0.5f);//말풍선 위치 고정
+    }
 
     void Update()
     {
-        //미남이를 터치했을때 랜덤으로 말풍선이 나옴
         if(Input.GetMouseButtonDown(0)){  //마우스 클릭
 	        touchPosition = Input.mousePosition;
             isClicked = true;
@@ -22,13 +32,27 @@ public class MinamTouch : MonoBehaviour
         Vector2 tp = Camera.main.ScreenToWorldPoint(touchPosition);
         RaycastHit2D hit = Physics2D.Raycast(tp, Vector2.zero);
 
-        if (hit.collider != null && isClicked)  //미남이 클릭했을때 터치이벤트 발생
+        if (hit.collider != null && isClicked)  //미남이 클릭했을때 말풍선 생김
         {
-            
-            Debug.Log("클릭");
-            isClicked = false; //터치 이벤트가 발생했으니 다음 프레임에선 발생하지 않도록 false로 변경해줌
-            //말풍선 이벤트 발생
+            isClicked = false;
+            SpeechBubble();  //말풍선 나옴
+        }
+        timer += Time.deltaTime;
+
+        if (timer >= hideDelay)// 4초 지나면 말풍선 숨기기
+        {
+            HideSpeechBubble();  //말풍선 숨기기
+            timer = 0f;  //타이머 초기화
         }
         
+    }
+    void SpeechBubble(){  //말풍선 보이게
+        BubbleText.text = MinamSay[Random.Range(0, MinamSay.Length)];  //랜덤으로 대사가 나옴
+        Bubble.SetActive(true);
+        timer = 0f;
+    }
+
+    void HideSpeechBubble(){  //말풍선 안보이게
+        Bubble.SetActive(false);
     }
 }
