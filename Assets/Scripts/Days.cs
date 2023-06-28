52df26dc237b3abe8545e7742cde841d5180c6ea
이지연 <leejiyeon415@gmail.com>
2023-06-29 01:34:36 +0900

    게이지바

---
+++ Assets/Scripts/Days.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Days : MonoBehaviour
+{
+    public static int day;
+    public Text DayText;  //날짜 텍스트
+    public Text DDayText;  //디데이 텍스트
+    void Update()
+    {
+        
+    }
+}
