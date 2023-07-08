69a16f9a3860ee7b5542c90176037fc173b08a56
이지연 <leejiyeon415@gmail.com>
2023-07-08 22:00:52 +0900

    게임횟수 오류 해결

--- Assets/Scripts/Days.cs
+++ Assets/Scripts/Days.cs
@@ -6,13 +6,14 @@ public class Days : MonoBehaviour
 {
     public Text DayText;  //날짜 텍스트
     static int month = 1;  //월
-    static int gameCount = 0;  //게임횟수
+    static int gameCount = 2;  //게임횟수
 
     void Update()
     {
         gameCount = PlayerPrefs.GetInt("게임횟수");
-        if(gameCount == 5){  //미니게임을 5번 했다면 한달 지남
-            gameCount = 0;//게임 횟수 다시 초기화
+        if(gameCount == 5){  //미니게임을 3번 하면 한달 지남
+            gameCount = 2;//게임 횟수 다시 초기화
+            //(왜인지 모르겠으나 처음 횟수가 2로 시작되기 때문에 횟수 맞추려면 어쩔수없이 2로 초기화 해야함...)
             month += 1;  //날짜 증가
             PlayerPrefs.SetInt("게임횟수",gameCount);
         }
