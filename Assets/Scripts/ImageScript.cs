2902e5570d9aec964abb9de8118222200dbc6f31
HyorimKang <s2219@e-mirim.hs.kr>
2023-07-10 04:45:56 +0900

    삭제된 ImageScript 다시 만들기

---
+++ Assets/Scripts/ImageScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageScript : MonoBehaviour
+{
+    public GameObject[] CorrectImage;  //정답 미남이 이미지
+    public GameObject[] WrongImage;  //오답 미남이 이미지
+    public Quiz quiz;
+    private int level; //현재 레벨
+    private int i;
+    
+
+    void Start(){
+        //level을 받아오기
+        PlayerPrefs.SetInt("레벨", level); 
+        WrongImage[level].SetActive(true);
+        //ChangeImage(level);
+    }
+    void Update(){
+        
+        //처음에는 모든 미남이 false만들기 
+        for(i = 0; i<3; i++){
+            CorrectImage[i].SetActive(false);
+            WrongImage[i].SetActive(false);
+        }
+
+        //정답, 오답에 따라 이미지 다르게 보이기
+        if(quiz.check){
+            CorrectImage[level].SetActive(true);
+            WrongImage[level].SetActive(false);
+        } 
+        else{
+            WrongImage[level].SetActive(true);
+            CorrectImage[level].SetActive(false);
+        }
+    }
+}
