4aa5ab58aed4aa1c1f02cf305c95d8337deb47aa
YeEun Park <komgolo67@naver.com>
2023-07-10 11:33:12 +0900

    a

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,50 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
- 
-    bool isTop = false;
-    bool isJump = false;
+    private bool isJump = false;
+    private bool isTop = false;
     public float jumpHeight = 0;
     public float jumpSpeed = 0;
 
-    Vector2 startPosition;
-    Animator animator;
-    void Start()
-    {
-        startPosition = transform.position;
-        animator = GetComponent<Animator>();
-    }
+    private Vector2 startPosition;
+    private Animator animator;
+void Start()
+{
+	startPosition = transform.position;
+    animator = GetComponent<Animator>();
+}
 
-
-    void Update()
-    {
-        animator.SetBool("run", true);
-        if(Input.GetMouseButtonDown(0))
+void Update()
+{
+	// 달리는 애니메이션으로 설정하기
+	animator.SetBool("run", true);
+        if (Input.GetKeyDown(KeyCode.Space)) // spacebar 누르면
         {
             isJump = true;
-        } else if ( transform.position.y <= startPosition.y)
+        }
+        else if (transform.position.y <= startPosition.y)
         {
             isJump = false;
             isTop = false;
             transform.position = startPosition;
         }
 
-        if(isJump)
+        if (isJump)
         {
-            if(transform.position.y <= jumpHeight - 0.1f && !isTop)
+            if (transform.position.y <= jumpHeight - 0.1f && !isTop)
             {
-                transform.position = Vector2.Lerp(transform.position, 
-                new Vector2(transform.position.x, jumpHeight), jumpSpeed * Time.deltaTime);
+                transform.position = Vector2.Lerp(transform.position,
+                    new Vector2(transform.position.x, jumpHeight), jumpSpeed * Time.deltaTime);
             }
             else
             {
                 isTop = true;
             }
-            if(transform.position.y > startPosition.y && isTop)
+
+            if (transform.position.y > startPosition.y && isTop)
             {
-                transform.position = Vector2.MoveTowards(transform.position, startPosition, jumpSpeed * Time.deltaTime);
+                transform.position = Vector2.MoveTowards(transform.position,
+                    startPosition, jumpSpeed * Time.deltaTime);
             }
-            
         }
     }
 }
