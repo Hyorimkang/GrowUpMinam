3ebc1cc76c3e923056740b215f728079c8f67d25
이지연 <leejiyeon415@gmail.com>
2023-07-10 10:48:11 +0900

    버튼 비활성화 코드 바꿈

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,27 +5,30 @@ using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour
 {
+
     public Button ExerciseButton;
     public Button DietButton;
     public Button StudyButton;
+    private void Start() {
+    }
     private void Update() 
     {
-        if(PlayerPrefs.GetInt("운동하기") == 0){  //아직 운동하기 게임을 플레이하지 않았다면 버튼 활성화
+        if(PlayerPrefs.GetString("운동하기버튼").Equals("활성화")){  //아직 운동하기 게임을 플레이하지 않았다면 버튼 활성화
             ExerciseButton.interactable = true;
         }
-        else if(PlayerPrefs.GetInt("운동하기") == 1){  //운동하기 게임을 플레이 했다면 버튼 비활성화
+        else if(PlayerPrefs.GetString("운동하기버튼").Equals("비활성화")){  //운동하기 게임을 플레이 했다면 버튼 비활성화
             ExerciseButton.interactable = false;
         }
-        if(PlayerPrefs.GetInt("식단하기") == 0){  //아직 식단하기 게임을 플레이하지 않았다면 버튼 활성화
+        if(PlayerPrefs.GetString("식단하기버튼").Equals("활성화")){  //아직 식단하기 게임을 플레이하지 않았다면 버튼 활성화
             DietButton.interactable = true;
         }
-        else if(PlayerPrefs.GetInt("식단하기") == 1){  //식단하기 게임을 플레이 했다면 버튼 비활성화
+        else if(PlayerPrefs.GetString("식단하기버튼").Equals("비활성화")){  //식단하기 게임을 플레이 했다면 버튼 비활성화
             DietButton.interactable = false;
         }
-        if(PlayerPrefs.GetInt("공부하기") == 0){  //아직 공부하기 게임을 플레이하지 않았다면 버튼 활성화
+        if(PlayerPrefs.GetString("공부하기버튼").Equals("활성화")){  //아직 공부하기 게임을 플레이하지 않았다면 버튼 활성화
             StudyButton.interactable = true;
         }
-        else if(PlayerPrefs.GetInt("공부하기") == 1){  //공부하기 게임을 플레이 했다면 버튼 비활성화
+        else if(PlayerPrefs.GetString("공부하기버튼").Equals("비활성화")){  //공부하기 게임을 플레이 했다면 버튼 비활성화
             StudyButton.interactable = false;
         }
     }
